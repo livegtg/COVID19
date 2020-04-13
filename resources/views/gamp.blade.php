14bd655cd7a45cdc351e12aa59a08702338d58e6
Juan Fajardo <bett0@potosi.bo>
2020-04-12 22:47:20 -0400

    Tema

---
+++ resources/views/gamp.blade.php
@@ -0,0 +1,326 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="webthemez">
+
+    <!-- Title -->
+    <title>G.A.M.Potosí</title>
+
+    <!-- CSS -->
+	 <link href="{{asset('asset/bootstrap/css/bootstrap.css')}}" rel="stylesheet">
+   <link href="{{asset('asset/css/styles.css')}}" rel="stylesheet">
+	 <link href="{{asset('asset/fonts/font-awesome/css/font-awesome.css')}}" rel="stylesheet">
+
+
+    <!-- Favicons -->
+    <link href="{{asset('asset/images/favicon/favicon.png')}}" rel="shortcut icon">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Modernizr Scripts -->
+    <script src="{{asset('asset/js/modernizr-2.7.1.min.js')}}"></script>
+
+  </head>
+  <body class="index" id="to-top">
+
+    <!-- Side nav -->
+    <nav class="side-nav" role="navigation">
+
+      <ul class="nav-side-nav">
+        <li><a class="tooltip-side-nav" href="#section-1" title="" data-original-title="Services" data-placement="left"></a></li>
+        <li><a class="tooltip-side-nav" href="#section-2" title="" data-original-title="Features" data-placement="left"></a></li>
+        <li><a class="tooltip-side-nav" href="#section-3" title="" data-original-title="Subscribe" data-placement="left"></a></li>
+        <li><a class="tooltip-side-nav" href="#to-top" title="" data-original-title="Back" data-placement="left"></a></li>
+      </ul>
+
+    </nav> <!-- /.side-nav -->
+
+    <!-- mainBanner -->
+    <header class="mainBanner" role="banner">
+
+      <div class="container">
+
+        <div class="row">
+
+          <div class="col-md-7 col-sm-7">
+
+            <!-- Logo -->
+            <figure>
+              <a href="./index.html">
+                <img class="img-logo" src="{{asset('asset/images/logo.png')}}" style="width:330px;" alt="">
+              </a>
+            </figure> <!-- /.text-center -->
+
+            <!-- Title -->
+            <h1>App COVID19 - Potosí</h1>
+
+            <!-- Sub title -->
+            <p>Esta aplicacion encontraran todo referente al COVID19 del <br> <b>Gobierno Autonomo Municipal de Potosí</b>  <br>
+            Infórmate sobre le coronavirus, llama al 165 o ingresa a <a href="https://www.boliviasegura.gob.bo/" target="_blank"></a>  </p>
+
+            <!-- Button -->
+            <div class="buttons-wrapper">
+					<a href="#" class="button">Descargar</a>
+
+				</div>
+				<!-- /.btn-app-store -->
+
+          </div> <!-- /.col-md-7 -->
+
+          <div class="col-md-5 col-sm-5">
+
+            <!-- Images showcase -->
+            <figure>
+              <img class="img-iPhone" src="{{asset('asset/images/iphone/2.svg')}}" alt="">
+            </figure>
+
+          </div> <!-- /.col-md-5 -->
+
+        </div> <!-- /.row -->
+
+      </div> <!-- /.container -->
+
+    </header> <!-- /.mainBanner -->
+
+
+
+
+    <!-- Services -->
+    <section class="highlights" id="section-1">
+
+      <div class="container">
+
+        <div class="row">
+
+          <div class="col-md-3 highlightsCol">
+            <figure>
+              <i class="fa fa-recycle" style="font-size:48px;color:#5EB920"></i>
+            </figure>
+            <h4>Cuidados</h4>
+            <p>Cuidados del COVID19</p>
+          </div>
+
+          <div class="col-md-3 highlightsCol">
+            <figure>
+              <i class="fa fa-laptop" style="font-size:48px;color:#5EB920"></i>
+            </figure>
+            <h4>Sintomas Y Autoevalacion</h4>
+            <p>Sintomas y autoevalacion para detectar el COVID19</p>
+          </div>
+
+          <div class="col-md-3 highlightsCol">
+            <figure>
+              <i class="fa fa-map-marker" style="font-size:48px;color:#5EB920"></i>
+            </figure>
+            <h4>Mapa</h4>
+            <p>Mapa estadistico de la infeccion</p>
+          </div>
+
+          <div class="col-md-3 highlightsCol">
+            <figure>
+              <i class="fa fa-mobile" style="font-size:48px;color:#5EB920"></i>
+            </figure>
+            <h4>Numero de contactos</h4>
+            <p>Numero de contacto para cualquier emergencia.</p>
+          </div>
+
+        </div> <!-- /.row -->
+
+      </div> <!-- /.container -->
+
+    </section> <!-- /.highlights -->
+
+
+
+
+    <!-- Features -->
+    <section class="ourFeatures" id="section-2">
+      <div class="row">
+			  <div class="col-lg-4 col-md-4">
+          <a href="{{asset('asset/images/emergencia1.jpg')}}"> <img src="{{asset('asset/images/emergencia1.jpg')}}" width="500" alt=""></a>
+        </div>
+        <div class="col-lg-4 col-md-4">
+          <a href="{{asset('asset/images/emergencia2.jpg')}}"> <img src="{{asset('asset/images/emergencia2.jpg')}}" width="500" alt=""></a>
+        </div>
+        <div class="col-lg-4 col-md-4">
+          <a href="{{asset('asset/images/emergencia3.jpg')}}"> <img src="{{asset('asset/images/emergencia3.jpg')}}" width="500" alt=""></a>
+        </div>
+      </div>
+      <br>
+      <div class="row">
+			  <div class="col-lg-4 col-md-4">
+          <a href="{{asset('asset/images/1.jpg')}}"> <img src="{{asset('asset/images/1.jpg')}}" width="500" alt=""></a>
+        </div>
+        <div class="col-lg-4 col-md-4">
+          <a href="{{asset('asset/images/2.jpg')}}"> <img src="{{asset('asset/images/2.jpg')}}" width="500" alt=""></a>
+        </div>
+        <div class="col-lg-4 col-md-4">
+          <a href="{{asset('asset/images/3.jpg')}}"> <img src="{{asset('asset/images/3.jpg')}}" width="500" alt=""></a>
+        </div>
+      </div>
+      <br>
+      <div class="row">
+			  <div class="col-lg-4 col-md-4">
+          <a href="{{asset('asset/images/4.jpg')}}"> <img src="{{asset('asset/images/4.jpg')}}" width="500" alt=""></a>
+        </div>
+        <div class="col-lg-4 col-md-4">
+          <a href="{{asset('asset/images/5.jpg')}}"> <img src="{{asset('asset/images/5.jpg')}}" width="500" alt=""></a>
+        </div>
+        <div class="col-lg-4 col-md-4">
+          <a href="{{asset('asset/images/6.jpg')}}"> <img src="{{asset('asset/images/6.jpg')}}" width="500" alt=""></a>
+        </div>
+      </div>
+
+
+      <br>
+      <br>
+    </section> <!-- /.ourFeatures -->
+
+
+    <!-- Subscribe -->
+    <section class="subscribe" id="section-3">
+
+      <div class="container">
+
+        <div class="row">
+          <h2>Numero de contactos</h2>
+          <div class="col-md-6">
+            <!-- Title -->
+
+            <!-- Subscribe form -->
+            <div class="row">
+
+              <div class="col-md-6 col-md-offset-3 subscribeCol">
+               <!--
+                <form class="subscribe-form form-inline" action="./index.html" role="form">
+                  <div class="form-group">
+                    <label class="sr-only" for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email address">
+                  </div>
+                  <button class="btn btn-prime" type="submit">Subscribe</button>
+                </form>
+                <section class="subscribe-description">
+                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry It was popularised in the 1960s with the release of Letraset.</p>
+                </section>-->
+
+              </div> <!-- /.col-md-6 -->
+
+              <div class="col-md-6 col-md-offset-3 subscribeCol">
+                <div class="col-md-12">
+                  <figure>
+                    <img src="{{asset('asset/images/defensoria.png')}}" alt=""> Defensoria 6262626
+                  </figure>
+                  <p></p>
+                </div>
+                <div class="col-md-12">
+                  <figure>
+                    <img src="{{asset('asset/images/intendencia.png')}}" alt=""> Intendencia 6262626
+                  </figure>
+                  <p></p>
+                </div>
+                <div class="col-md-12">
+                  <figure>
+                    <img src="{{asset('asset/images/reten.png')}}" alt=""> Detenidos 6262626
+                  </figure>
+                  <p></p>
+                </div>
+                <div class="col-md-12">
+                  <figure>
+                    <img src="{{asset('asset/images/atc.png')}}" alt=""> Contacto 6262626
+                  </figure>
+                  <p></p>
+                </div>
+
+
+              </div> <!-- /.col-md-6 -->
+
+            </div> <!-- /.row -->
+
+          </div> <!-- /.col-md-12 -->
+
+
+          <div class="col-md-6">
+            <!-- Title -->
+
+            <!-- Subscribe form -->
+            <div class="row">
+
+              <div class="col-md-6 col-md-offset-3 subscribeCol">
+                <div class="col-md-12">
+                  <figure>
+                    <img src="{{asset('asset/images/red_ambu.png')}}" alt=""> Ambulancia 6262626
+                  </figure>
+                  <p></p>
+                </div>
+                <div class="col-md-12">
+                  <figure>
+                    <img src="{{asset('asset/images/guardia.png')}}" alt=""> Guardia 6262626
+                  </figure>
+                  <p></p>
+                </div>
+                <div class="col-md-12">
+                  <figure>
+                    <img src="{{asset('asset/images/quejas_basur.png')}}" alt=""> EMAP 6262626
+                  </figure>
+                  <p></p>
+                </div>
+                <div class="col-md-12">
+                  <figure>
+                    <img src="{{asset('asset/images/guardia.png')}}" alt=""> Guardia 6262626
+                  </figure>
+                  <p></p>
+                </div>
+
+
+              </div> <!-- /.col-md-6 -->
+
+            </div> <!-- /.row -->
+
+          </div> <!-- /.col-md-12 -->
+
+
+
+
+        </div> <!-- /.row -->
+
+      </div> <!-- /.container -->
+
+    </section> <!-- /.subscribe -->
+
+
+
+
+    <!-- Footer -->
+    <footer class="footer-section" role="contentinfo">
+	        <div class="container">
+	            <div class="row">
+	                <div class="col-sm-7 footer-copyright">
+	                    <p> Gobierno Autnomo Municipal de Potosí - Support<a href="https://webthemez.com/tag/free" target="_blank"> by WebThemez.com</a> </p>
+	                </div>
+	                <div class="col-sm-5 footer-social">
+	                    <div class="social-icons">
+                        <a href="https://www.facebook.com/gobiernomunicipaldepotosi/" target="_blank"><i class="fa fa-facebook"></i></a>
+                        <!--<a href="https://twitter.com/"><i class="fa fa-twitter"></i></a>-->
+						            <!--<a href="https://www.google.com/"><i class="fa fa-google-plus"></i></a>-->
+                        <!--<a href="https://www.dribbble.com/"><i class="fa fa-dribbble"></i></a>-->
+                        <!--<a href="https://www.linkedin.com/"><i class="fa fa-linkedin-square"></i></a>-->
+						          </div>
+	                </div>
+	            </div>
+	        </div>
+        </footer>
+
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="{{asset('asset/js/jquery-2.1.0.min.js')}}"></script>
+    <script src="{{asset('asset/bootstrap/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('asset/js/customScripts.js')}}"></script>
+  </body>
+</html>
